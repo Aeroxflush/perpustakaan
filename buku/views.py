bc931fb8b546e8fe573d583489eb86f8e5931866
Aeroxflush <ragielgerorius@gmail.com>
2025-01-15 10:34:46 +0700

    update (nambah form input pada app buku)

--- buku/views.py
+++ buku/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Book
+from django.http import HttpResponse
 
 def index(request):
     buku = Book.objects.all()
@@ -9,3 +10,25 @@ def index(request):
         'buku': buku,
     }
     return render(request, "buku.html" , context)
+
+def bukuadd(request):
+    context ={
+        'Title' : 'Perpustakaan STMIK Pontianak' ,
+        'Heading' : 'Form Pengisian Data Buku' ,
+    }
+    return render(request, 'bukuadd.html' ,
+    context)
+
+def process_bukuadd(request):
+    if request.method == 'POST':
+        kode_buku = request.POST.get('kode_buku')
+        judul_buku = request.POST.get('judul_buku')
+        pengarang = request.POST.get('pengarang')
+        penerbit = request.POST.get('penerbit')
+        tahun = request.POST.get('tahun')
+
+        buku = Book(kode_buku=kode_buku, judul_buku=judul_buku, pengarang=pengarang, penerbit=penerbit, tahun=tahun)
+        buku.save()
+        return redirect('../../buku/')
+    else: 
+        return HttpResponse("Invalid request method.")
