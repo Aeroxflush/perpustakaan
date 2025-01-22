24d420dd04e1b9d17139bdd25c40b7168ece8caa
Aeroxflush <ragielgerorius@gmail.com>
2025-01-22 12:12:23 +0700

    update (nambah header di tabel django admin)

--- buku/admin.py
+++ buku/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-
 from .models import Book
-admin.site.register(Book)
+
+@admin.register(Book)
+class BukuAdmin(admin.ModelAdmin):
+    list_display = ('kode_buku', 'judul_buku', 'pengarang', 'penerbit', 'tahun')
+    search_fields = ('judul_buku', 'pengarang', 'penerbit', 'tahun')
+    list_filter = ('judul_buku', 'pengarang', 'penerbit', 'tahun')
